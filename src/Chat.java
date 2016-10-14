839384e542e69c93c8a148a7ea62d9f549988424
Sam <samtehg@yahoo.com>
2016-10-13 20:48:48 -0500

    Initial

---
+++ src/Chat.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+
+public class Chat extends JFrame {
+    private Chat() {
+        super("Chat");
+        try {
+            SwingUtilities.invokeAndWait(() -> {
+                setPreferredSize(new Dimension(800, 600));
+                setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                setResizable(false);
+                pack();
+                setVisible(true);
+                setLocationRelativeTo(null);
+            });
+        } catch (InterruptedException | InvocationTargetException e) {e.printStackTrace();}
+    }
+
+    public static void main(String[] args) {
+        new Chat();
+    }
+}
